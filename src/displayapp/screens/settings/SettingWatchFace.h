4640b4aa9f3fc789ea1f53a785aa1bffabb6bb01
costinmarinescu <87228701+costinmarinescu@users.noreply.github.com>
2025-10-15 10:43:53 +0200

    Update SettingWatchFace.h

--- src/displayapp/screens/settings/SettingWatchFace.h
+++ src/displayapp/screens/settings/SettingWatchFace.h
@@ -1,53 +1,34 @@
 #pragma once
 
-#include <array>
 #include <cstdint>
 #include <lvgl/lvgl.h>
-
-#include "displayapp/screens/ScreenList.h"
 #include "components/settings/Settings.h"
 #include "displayapp/screens/Screen.h"
-#include "displayapp/screens/Symbols.h"
-#include "displayapp/screens/CheckboxList.h"
-#include "displayapp/screens/WatchFaceInfineat.h"
-#include "displayapp/screens/WatchFaceCasioStyleG7710.h"
+#include "components/fs/FS.h"
 
 namespace Pinetime {
-
   namespace Applications {
     namespace Screens {
-
       class SettingWatchFace : public Screen {
       public:
-        struct Item {
-          const char* name;
-          WatchFace watchface;
-          bool enabled;
-        };
-
         SettingWatchFace(DisplayApp* app,
-                         std::array<Item, UserWatchFaceTypes::Count>&& watchfaceItems,
-                         Pinetime::Controllers::Settings& settingsController,
-                         Pinetime::Controllers::FS& filesystem);
+                        Pinetime::Controllers::Settings& settingsController,
+                        Pinetime::Controllers::FS& filesystem);
         ~SettingWatchFace() override;
 
-        bool OnTouchEvent(TouchEvents event) override;
+        void Refresh() override {}
 
       private:
-        auto CreateScreenList() const;
-        std::unique_ptr<Screen> CreateScreen(unsigned int screenNum) const;
-
-        static constexpr int settingsPerScreen = 4;
-        std::array<Item, UserWatchFaceTypes::Count> watchfaceItems;
-        static constexpr int nScreens = UserWatchFaceTypes::Count > 0 ? (UserWatchFaceTypes ::Count - 1) / settingsPerScreen + 1 : 1;
-
-        Controllers::Settings& settingsController;
+        DisplayApp* app;
+        Pinetime::Controllers::Settings& settingsController;
         Pinetime::Controllers::FS& filesystem;
 
-        static constexpr const char* title = "Watch face";
-        static constexpr const char* symbol = Symbols::home;
+        static constexpr std::size_t nScreens = 7;  // CHANGE FROM 6 TO 7
 
-        ScreenList<nScreens> screens;
+        lv_obj_t* cbOption[nScreens];  // CHANGE ARRAY SIZE
+        uint32_t currentIndex;
+
+        static void event_handler(lv_obj_t* obj, lv_event_t event);
       };
     }
   }
