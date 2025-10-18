52c1f2e1a490ac4320a9e6855307850f59eac417
costinmarinescu <87228701+costinmarinescu@users.noreply.github.com>
2025-10-18 23:17:25 +0200

    Update BioAgeCalculator.h

--- src/components/praxiom/BioAgeCalculator.h
+++ src/components/praxiom/BioAgeCalculator.h
@@ -11,13 +11,21 @@ namespace Pinetime {
         bool dataValid;
       };
 
-      uint8_t CalculateBioAge(uint8_t chronoAge, uint8_t restingHR, uint8_t hrv, uint16_t steps, uint8_t sleepScore);
-      void UpdateMetrics(/* controllers */);
+      uint8_t CalculateBioAge(uint8_t chronoAge,
+                              uint8_t restingHR,
+                              uint8_t hrv,
+                              uint16_t steps,
+                              uint8_t sleepScore);
+      void UpdateMetrics();
       BioAgeData GetBioAgeData() const;
       void Reset();
     private:
-      BioAgeData data_;
-      uint8_t ComputeWeightedBioAge(uint8_t chronoAge, uint8_t hrvScore, uint8_t activityScore, uint8_t sleepScore, uint8_t stressScore);
+      BioAgeData data_{};
+      uint8_t ComputeWeightedBioAge(uint8_t chronoAge,
+                                    uint8_t hrvScore,
+                                    uint8_t activityScore,
+                                    uint8_t sleepScore,
+                                    uint8_t stressScore);
     };
   }
 }
