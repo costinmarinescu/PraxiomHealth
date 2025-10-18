d637ce1ce2ec12f98ffa474553cd6f0f8d09906a
costinmarinescu <87228701+costinmarinescu@users.noreply.github.com>
2025-10-18 10:27:50 +0200

    Add files via upload

---
+++ src/Version.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Pinetime {
+  class Version {
+    public:
+      static constexpr const char* version = "1.0.0";
+      static constexpr const char* gitCommitHash = "praxiomhealth";
+      static constexpr const int major = 1;
+      static constexpr const int minor = 0;
+      static constexpr const int patch = 0;
+  };
+}
