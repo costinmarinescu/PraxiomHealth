35ca56224ab773e37dabec62dccf42a663d5a2e8
costinmarinescu <87228701+costinmarinescu@users.noreply.github.com>
2025-10-18 10:27:22 +0200

    Add files via upload

---
+++ src/displayapp/apps/Apps.h
@@ -0,0 +1,44 @@
+#pragma once
+
+namespace Pinetime {
+  namespace Applications {
+    enum class Apps {
+      None,
+      Launcher,
+      Clock,
+      FirmwareValidation,
+      Notifications,
+      HeartRate,
+      Navigation,
+      Music,
+      Paddle,
+      Tile,
+      Twos,
+      Paint,
+      Metronome,
+      Motion,
+      Steps,
+      Weather,
+      PassKey,
+      QuickSettings,
+      Settings,
+      SettingWatchFace,
+      SettingTimeFormat,
+      SettingDisplay,
+      SettingWakeUp,
+      SettingSteps,
+      SettingSetDate,
+      SettingSetTime,
+      SettingChimes,
+      SettingShakeThreshold,
+      SettingBluetooth,
+      BatteryInfo,
+      SysInfo,
+      FlashLight,
+      StopWatch,
+      Alarm,
+      Timer,
+      Twos2048
+    };
+  }
+}
