b6034ed6288a3650a6d95ff08f8a5afae4aa679c
costinmarinescu <87228701+costinmarinescu@users.noreply.github.com>
2025-10-18 23:01:30 +0200

    Create Apps.h

---
+++ src/displayapp/Apps.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Pinetime {
+  namespace Applications {
+    enum class Apps : uint8_t {
+      BioAge,
+      Settings,
+      Alarm,
+      Stopwatch,
+      __COUNT
+    };
+  }
+}
