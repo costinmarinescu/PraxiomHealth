887002b46388e69f4a6d4917a96dc31fa6c6fe9e
costinmarinescu <87228701+costinmarinescu@users.noreply.github.com>
2025-10-18 23:01:43 +0200

    Update Apps.h

--- src/displayapp/apps/Apps.h
+++ src/displayapp/apps/Apps.h
@@ -2,43 +2,12 @@
 
 namespace Pinetime {
   namespace Applications {
-    enum class Apps {
-      None,
-      Launcher,
-      Clock,
-      FirmwareValidation,
-      Notifications,
-      HeartRate,
-      Navigation,
-      Music,
-      Paddle,
-      Tile,
-      Twos,
-      Paint,
-      Metronome,
-      Motion,
-      Steps,
-      Weather,
-      PassKey,
-      QuickSettings,
+    enum class Apps : uint8_t {
+      BioAge,
       Settings,
-      SettingWatchFace,
-      SettingTimeFormat,
-      SettingDisplay,
-      SettingWakeUp,
-      SettingSteps,
-      SettingSetDate,
-      SettingSetTime,
-      SettingChimes,
-      SettingShakeThreshold,
-      SettingBluetooth,
-      BatteryInfo,
-      SysInfo,
-      FlashLight,
-      StopWatch,
       Alarm,
-      Timer,
-      Twos2048
+      Stopwatch,
+      __COUNT
     };
   }
 }
