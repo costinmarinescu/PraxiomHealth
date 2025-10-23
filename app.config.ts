d7f58a0be19ee2b225d5ffc1c88e887f665c8f9e
costinmarinescu <87228701+costinmarinescu@users.noreply.github.com>
2025-10-23 10:38:40 +0200

    Create  app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,44 @@
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "Praxiom Health",
+  slug: "praxiom-health",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#FFA500"
+  },
+  plugins: [
+    [
+      "expo-ble-plx",
+      {
+        isBackgroundEnabled: true,
+        modes: ["peripheral", "central"],
+        bluetoothAlwaysPermission: "Allow Praxiom Health to connect to your PineTime watch"
+      }
+    ]
+  ],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.praxiom.health"
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#FFA500"
+    },
+    package: "com.praxiom.health",
+    permissions: [
+      "BLUETOOTH",
+      "BLUETOOTH_ADMIN",
+      "BLUETOOTH_CONNECT",
+      "BLUETOOTH_SCAN",
+      "ACCESS_FINE_LOCATION"
+    ]
+  }
+});
