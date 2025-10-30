197f3b4af005c5d0a6878a326f999e53a81c6a93
costinmarinescu <87228701+costinmarinescu@users.noreply.github.com>
2025-10-30 20:01:02 +0100

    Update WatchScreen.js

--- screens/WatchScreen.js
+++ screens/WatchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -24,7 +24,6 @@ const WatchScreen = () => {
 
   const handleConnect = () => {
     setIsScanning(true);
-    // Simulate BLE scanning
     setTimeout(() => {
       setIsScanning(false);
       setIsConnected(true);
