2d078192328092b0750f81ef1c1e3043c21308f6
costinmarinescu <87228701+costinmarinescu@users.noreply.github.com>
2025-11-10 14:56:38 +0100

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { ImageBackground, StyleSheet } from 'react-native';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
+import { AppContextProvider } from './AppContext';
 import DashboardScreen from './screens/DashboardScreen';
 import WatchScreen from './screens/WatchScreen';
 import SettingsScreen from './screens/SettingsScreen';
@@ -19,41 +20,43 @@ const MyTheme = {
 
 export default function App() {
   return (
-    <ImageBackground 
-      source={require('./assets/praxiom_background.png')}
-      style={styles.backgroundImage}
-      resizeMode="cover"
-    >
-      <NavigationContainer theme={MyTheme}>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              if (route.name === 'Dashboard') {
-                iconName = focused ? 'home' : 'home-outline';
-              } else if (route.name === 'Watch') {
-                iconName = focused ? 'watch' : 'watch-outline';
-              } else if (route.name === 'Settings') {
-                iconName = focused ? 'settings' : 'settings-outline';
-              }
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            headerShown: false,
-            tabBarActiveTintColor: '#00CFC1',
-            tabBarInactiveTintColor: 'white',
-            tabBarStyle: {
-              backgroundColor: 'rgba(0, 0, 0, 0.5)',
-              borderTopWidth: 0,
-              elevation: 0,
-            },
-          })}
-        >
-          <Tab.Screen name="Dashboard" component={DashboardScreen} />
-          <Tab.Screen name="Watch" component={WatchScreen} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </ImageBackground>
+    <AppContextProvider>
+      <ImageBackground 
+        source={require('./assets/praxiom_background.png')}
+        style={styles.backgroundImage}
+        resizeMode="cover"
+      >
+        <NavigationContainer theme={MyTheme}>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                if (route.name === 'Dashboard') {
+                  iconName = focused ? 'home' : 'home-outline';
+                } else if (route.name === 'Watch') {
+                  iconName = focused ? 'watch' : 'watch-outline';
+                } else if (route.name === 'Settings') {
+                  iconName = focused ? 'settings' : 'settings-outline';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              headerShown: false,
+              tabBarActiveTintColor: '#00CFC1',
+              tabBarInactiveTintColor: 'white',
+              tabBarStyle: {
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                borderTopWidth: 0,
+                elevation: 0,
+              },
+            })}
+          >
+            <Tab.Screen name="Dashboard" component={DashboardScreen} />
+            <Tab.Screen name="Watch" component={WatchScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ImageBackground>
+    </AppContextProvider>
   );
 }
 
